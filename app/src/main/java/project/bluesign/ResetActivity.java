edb837081ce492c801c6542e361691a0d7cb26ad
HansTheRisk <mat2236@gmail.com>
2016-10-09 13:40:21 +0100

    Small tweak

--- app/src/main/java/project/bluesign/ResetActivity.java
+++ app/src/main/java/project/bluesign/ResetActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class ResetActivity extends AppCompatActivity {
 
@@ -20,7 +21,10 @@ public class ResetActivity extends AppCompatActivity {
         if (TextUtils.isEmpty(pin.getText().toString()))
             pin.setError("Don't forget about the pin!");
         else {
-            getSharedPreferences("UserInfo", 0).edit().clear().commit(); //Verify user pin here!
+            if (getSharedPreferences("UserInfo", 0).edit().clear().commit()) { //Verify user pin here!
+                Toast.makeText(this, "Settings reset", Toast.LENGTH_SHORT).show();
+                finish();
+            }
         }
     }
 }
